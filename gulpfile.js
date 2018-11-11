6e7233f348c1a3ccd3bf73f3fd0c80b02500e8e2
Nazar1508 <nazar-koltsun@ukr.net>
2018-11-11 12:38:22 +0200

    вирішив проблеми з webp

--- gulpfile.js
+++ gulpfile.js
@@ -59,7 +59,7 @@ gulp.task("images", function () {
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("img"));
+    .pipe(gulp.dest("source/img/created-webp"));
 });
 
 gulp.task("html", function () {
